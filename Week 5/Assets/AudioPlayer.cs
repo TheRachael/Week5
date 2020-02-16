4bbef5d4cfeca123736379c90b556ecf617fb275
Rachael <rc690616@ohio.edu>
2020-02-15 19:40:09 -0500

    Uploaded

---
+++ Week 5/Assets/AudioPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    public AudioSource buttonAudioSource;
+    public void OnButtonClicked ()
+    {
+        if (buttonAudioSource.isPlaying)
+        {
+            buttonAudioSource.Stop();
+        }
+        else
+        {
+            buttonAudioSource.Play();
+        }
+
+    }
+}
